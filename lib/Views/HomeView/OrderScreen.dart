3595fd4891c18eef4fb9677eb4cb20d7e8c9ddcc
MuhammadHammad77 <hammadop74@gmail.com>
2024-08-04 19:50:39 +0500

    Final Hackathon Project

--- lib/Views/HomeView/OrderScreen.dart
+++ lib/Views/HomeView/OrderScreen.dart
@@ -5,6 +5,47 @@ class Orderscreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            child: Image.asset(
+              'assets/images/Background Image.png',
+              fit: BoxFit.cover,
+            ),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+          ),
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 30, left: 20),
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.white,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 10, left: 20),
+                  child: Text(
+                    'Order detail',
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Image.asset('assets/images/product 1.png'),
+                Image.asset('assets/images/product 2.png'),
+                Image.asset('assets/images/product 3.png'),
+                Center(child: Image.asset('assets/images/subtotal section.png'))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
