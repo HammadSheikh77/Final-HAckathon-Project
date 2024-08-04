12868595a1f6e902961126500658063bb9ef0911
MuhammadHammad77 <hammadop74@gmail.com>
2024-08-04 14:11:59 +0500

    Final Hackathon Project

--- lib/Views/auth/LoginScreen.dart
+++ lib/Views/auth/LoginScreen.dart
@@ -1,19 +1,182 @@
+import 'package:final_hackathon/ViewModels/Login_Controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../ViewModels/Password_Controller.dart';
 
 class Loginscreen extends StatelessWidget {
   const Loginscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    LoginController controller = Get.put(LoginController());
+
+    final PasswordController passwordcontroller = Get.put(PasswordController());
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Image.asset('assets/images/Background.png'),
-            ],
-          ),
-        ],
+      backgroundColor: const Color(0xff0B1225),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 80),
+                child: Image.asset('assets/images/logo (1).png'),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Text(
+                'Deliver Favourite Food',
+                style: TextStyle(
+                    fontSize: 21,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Container(
+                height: 398,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: const Color(0xff1D102D),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(top: 15),
+                      child: Text(
+                        'Login',
+                        style: const TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Form(
+                      key: controller.formKey,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 10, right: 15, left: 15),
+                            child: TextFormField(
+                              controller: controller.emailController,
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                    borderSide:
+                                        const BorderSide(color: Colors.white),
+                                  ),
+                                  focusedBorder: const OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                    Radius.circular(20),
+                                  )),
+                                  hintText: 'Enter Your Email',
+                                  hintStyle: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w400),
+                                  prefixIcon: const Icon(
+                                    Icons.email_outlined,
+                                    color: Colors.white,
+                                  )),
+                              validator: controller.validateEmail,
+                            ),
+                          ),
+                          Obx(
+                            () => Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 15, left: 15, right: 15),
+                              child: TextFormField(
+                                controller: controller.passwordController,
+                                obscureText:
+                                    passwordcontroller.isPasswordHidden.value,
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                      borderSide:
+                                          const BorderSide(color: Colors.white),
+                                    ),
+                                    hintText: 'Enter Your Password',
+                                    hintStyle:
+                                        const TextStyle(color: Colors.white),
+                                    prefixIcon: const Icon(
+                                      Icons.lock_open_outlined,
+                                      color: Colors.white,
+                                    ),
+                                    suffixIcon: IconButton(
+                                        onPressed: () {
+                                          passwordcontroller
+                                              .togglePasswordVisibility();
+                                        },
+                                        icon: Icon(
+                                          passwordcontroller
+                                                  .isPasswordHidden.value
+                                              ? Icons.visibility_off
+                                              : Icons.visibility,
+                                          color: Colors.white,
+                                        ))),
+                                validator: controller.validatePassword,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: 5,right: 10),
+                          child: const Text(
+                            'Forget Password?',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 25),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white54,
+                            minimumSize: Size(
+                              MediaQuery.of(context).size.width * 0.6,
+                              MediaQuery.of(context).size.height * 0.07,
+                            ),
+                          ),
+                          onPressed: () {
+                            if (controller.formKey.currentState!.validate()) {}
+                          },
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Text('Or',style: TextStyle(fontSize: 12,color: Color(0xffEC2578)),),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/icons/google-icon 1.png'),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width*0.03
+                          ),
+                          Image.asset('assets/icons/facebook-3 1.png'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
